21620e8f46e320e8065b556056e5691ae31af2ef
rafaelariganti <103071326+rafaelariganti@users.noreply.github.com>
2025-02-26 18:10:20 -0300

    Add files via upload

---
+++ PTesteClasses/PTesteClasses/Empregado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTesteClasses
+{
+    internal class Empregado
+    {
+        private int matricula;
+        private string nomeEmpregado;
+        private DateTime dataEntradaEmpresa;
+
+    public int Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public string NomeEmpregado
+        {
+            get
+            {
+                return nomeEmpregado;
+            }
+            set { nomeEmpregado = value; }
+        }
+
+        public DateTime DataEntradaEmpresa
+        {
+            get { return dataEntradaEmpresa; }
+            set
+            {
+                dataEntradaEmpresa = value;
+            }
+        }
+    }
+}
